dc0c87856af1ee4e578c60b53d53a6c56803173b
Sebastian <2354622+undercom@users.noreply.github.com>
2018-07-28 11:23:14 +0300

    Initial commit

---
+++ src/main/java/com/aeki/AEKI/AekiApplication.java
@@ -0,0 +1,12 @@
+package com.aeki.AEKI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AekiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aeki/AEKI/AekiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aeki.AEKI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AekiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
