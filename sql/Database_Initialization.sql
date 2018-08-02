5bc50af87f54ecc69507a4e5917aaa357da233e9
Sebastian <2354622+undercom@users.noreply.github.com>
2018-08-02 18:17:51 +0300

    Fix SQL Db Init DB name

--- sql/Database_Initialization.sql
+++ sql/Database_Initialization.sql
@@ -1,22 +1,22 @@
-DROP DATABASE test1;
+DROP DATABASE aeki;
 
-CREATE DATABASE test1;
+CREATE DATABASE aeki;
 
-USE test1;
+USE aeki;
 
-CREATE TABLE test1.dbo.Customer (
+CREATE TABLE aeki.dbo.Customer (
 	id int IDENTITY(1,1) NOT NULL,
-	Name nvarchar(20) DEFAULT (NULL),
+	Name nvarchar(50) DEFAULT (NULL),
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE test1.dbo.FlavorType (
+CREATE TABLE aeki.dbo.FlavorType (
 	id int IDENTITY(1,1) NOT NULL,
 	FlavorName nvarchar(50) NOT NULL,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE test1.dbo.Membership (
+CREATE TABLE aeki.dbo.Membership (
 	id int IDENTITY(1,1) NOT NULL,
 	ProductCategoryID int NOT NULL,
 	customerId int NOT NULL,
@@ -24,14 +24,14 @@ CREATE TABLE test1.dbo.Membership (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE test1.dbo.OrderList (
+CREATE TABLE aeki.dbo.OrderList (
 	orderId int IDENTITY(1,1) NOT NULL,
 	name varchar(50),
 	customerId int,
 	PRIMARY KEY (orderId)
 );
 
-CREATE TABLE test1.dbo.Product (
+CREATE TABLE aeki.dbo.Product (
 	id int IDENTITY(1,1) NOT NULL,
 	ProductCategoryID int NOT NULL,
 	ProductName nvarchar(50) NOT NULL,
@@ -43,14 +43,14 @@ CREATE TABLE test1.dbo.Product (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE test1.dbo.ProductCategories (
+CREATE TABLE aeki.dbo.ProductCategories (
 	id int IDENTITY(1,1) NOT NULL,
 	CategoryName varchar(50) NOT NULL,
 	ProductDetailsID int NOT NULL,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE test1.dbo.ProductDetails (
+CREATE TABLE aeki.dbo.ProductDetails (
 	id int IDENTITY(1,1) NOT NULL,
 	Number nvarchar(50),
 	Weight nvarchar(50),
@@ -59,34 +59,34 @@ CREATE TABLE test1.dbo.ProductDetails (
 	PRIMARY KEY (id)
 );
 
-INSERT INTO test1.dbo.Customer(Name) VALUES (N'Sebi');
+INSERT INTO aeki.dbo.Customer(Name) VALUES (N'Sebi');
 
-INSERT INTO test1.dbo.Customer(Name) VALUES (N'Claudiu');
+INSERT INTO aeki.dbo.Customer(Name) VALUES (N'Claudiu');
 
-INSERT INTO test1.dbo.Customer(Name) VALUES (N'Razvan');
+INSERT INTO aeki.dbo.Customer(Name) VALUES (N'Razvan');
 
-INSERT INTO test1.dbo.FlavorType(FlavorName) VALUES (N'Chili');
+INSERT INTO aeki.dbo.FlavorType(FlavorName) VALUES (N'Chili');
 
-INSERT INTO test1.dbo.FlavorType(FlavorName) VALUES (N'Sweet');
+INSERT INTO aeki.dbo.FlavorType(FlavorName) VALUES (N'Sweet');
 
-INSERT INTO test1.dbo.Membership(ProductCategoryID, customerId, discount) VALUES (1, 1, 20);
+INSERT INTO aeki.dbo.Membership(ProductCategoryID, customerId, discount) VALUES (1, 1, 20);
 
-INSERT INTO test1.dbo.OrderList(name, customerId) VALUES (N'Order 1', 1);
+INSERT INTO aeki.dbo.OrderList(name, customerId) VALUES (N'Order 1', 1);
 
-INSERT INTO test1.dbo.Product(ProductCategoryID, ProductName, Amount, Description, Price, DiscountPrice, OrderId) VALUES (1, N'Chair', 2, N'Big chair', 10, 20, 1);
+INSERT INTO aeki.dbo.Product(ProductCategoryID, ProductName, Amount, Description, Price, DiscountPrice, OrderId) VALUES (1, N'Chair', 2, N'Big chair', 10, 20, 1);
 
-INSERT INTO test1.dbo.Product(ProductCategoryID, ProductName, Amount, Description, Price, DiscountPrice, OrderId) VALUES (2, N'Hot Dog', 1, N'Small Hot Dog with Ketckup', 5, 15, 1);
+INSERT INTO aeki.dbo.Product(ProductCategoryID, ProductName, Amount, Description, Price, DiscountPrice, OrderId) VALUES (2, N'Hot Dog', 1, N'Small Hot Dog with Ketckup', 5, 15, 1);
 
-INSERT INTO test1.dbo.ProductCategories(CategoryName, ProductDetailsID) VALUES (N'Furniture', 1);
+INSERT INTO aeki.dbo.ProductCategories(CategoryName, ProductDetailsID) VALUES (N'Furniture', 1);
 
-INSERT INTO test1.dbo.ProductCategories(CategoryName, ProductDetailsID) VALUES (N'Textile', 2);
+INSERT INTO aeki.dbo.ProductCategories(CategoryName, ProductDetailsID) VALUES (N'Textile', 2);
 
-INSERT INTO test1.dbo.ProductCategories(CategoryName, ProductDetailsID) VALUES (N'Hot Dogs', 3);
+INSERT INTO aeki.dbo.ProductCategories(CategoryName, ProductDetailsID) VALUES (N'Hot Dogs', 3);
 
-INSERT INTO test1.dbo.ProductDetails(Number, Weight, Color, FlavorTypeID) VALUES (N'1', N'5', null, null);
+INSERT INTO aeki.dbo.ProductDetails(Number, Weight, Color, FlavorTypeID) VALUES (N'1', N'5', null, null);
 
-INSERT INTO test1.dbo.ProductDetails(Number, Weight, Color, FlavorTypeID) VALUES (N'2', null, N'Red', null);
+INSERT INTO aeki.dbo.ProductDetails(Number, Weight, Color, FlavorTypeID) VALUES (N'2', null, N'Red', null);
 
-ALTER TABLE test1.dbo.OrderList
+ALTER TABLE aeki.dbo.OrderList
 	ADD FOREIGN KEY (customerId)
 	REFERENCES dbo.Customer (id)
